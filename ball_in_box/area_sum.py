25229ec61f4a23b6b3bf77a0acdb2eb97d69e09b
wqqpcy <540436756@qq.com>
2018-05-29 23:24:45 +0800

    wqq: GUI,change area_sum

--- ball_in_box/area_sum.py
+++ ball_in_box/area_sum.py
@@ -1,44 +1,45 @@
 import math
 import ballinbox as bb
 import validate as val
-from tkinter import * 
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse,Circle
 
 def area_sum(circles):
-    area = 0.0
-    for circle in circles:
-        area += circle[2]**2 * math.pi
+	area = 0.0
+	for circle in circles:
+		area += circle[2]**2 * math.pi
 
-    return area
+	return area
 
 if __name__ == '__main__':
-    num_of_circle = 5
-    blockers = [(0.5, 0.5)
-               ,(0.5, -0.3)]
-    
-    circles = bb.ball_in_box(num_of_circle, blockers)
-    
-    if num_of_circle == len(circles) and val.validate(circles, blockers):
-        area = area_sum(circles)
-        print("Total area: {}".format(area))
-    else:
-        print("Error: no good circles.")
-    
+	num_of_circle = 5
+	blockers = [(0.5, 0.5)
+				,(0.5, -0.3)]
+	
+	circles = bb.ball_in_box(num_of_circle, blockers)
+
+	if num_of_circle == len(circles) and val.validate(circles, blockers):
+		area = area_sum(circles)
+		print("Total area: {}".format(area))
+	else:
+		print("Error: no good circles.")
+	
     #画图
-    canvas_width = 500
-    canvas_height =500
-    master = Tk()
-    master.title("ball in box")
-    w = Canvas(master, width=canvas_width, height=canvas_height)
-   
-    w.pack()
-    w.create_rectangle(50, 50, 450, 450, outline = "black")
-    for block in blockers:
-        w.create_oval(250+block[0]*200-2,250-block[1]*200-2,250+block[0]*200+2,250-block[1]*200+2,fill='red')
-    for circle in circles:
-        w.create_oval(250+(circle[0]-circle[2])*200,250-(circle[1]+circle[2])*200,250+(circle[0]+circle[2])*200,250-(circle[1]-circle[2])*200)
-
-    mainloop()
-
-
-
-
+	x1 = [-1,1]
+	x2 = [-1,-1]
+	x3 = [1,1]
+	y1 = [-1,1]
+	y2 = [-1,-1]
+	y3 = [1,1]
+	fig = plt.figure()
+	ax = fig.add_subplot(111)
+	for blocker in blockers:
+		plt.scatter(blocker[0],blocker[1],c = 36,marker = '*')
+	for circle in circles:
+		cir = Circle(xy = (circle[0],circle[1]),radius = circle[2],alpha = 0.8)
+		ax.add_patch(cir)
+	plt.plot(x1,y2)
+	plt.plot(x1,y3)
+	plt.plot(x2,y1)
+	plt.plot(x3,y1)
+	plt.show()
